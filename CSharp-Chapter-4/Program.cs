adedc971ae8eb50b5629f6404f25b1e49f672108
Paul Moreland <Paul.Moreland@edu.lidkoping.se>
2021-09-20 13:38:17 +0200

    Add project files.

---
+++ CSharp-Chapter-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Chapter_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chapter 4 - While & For");
+        }
+    }
+}
